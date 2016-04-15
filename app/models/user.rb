667afb41eb61e388ecab285e8c5fd7bcb7203fc1
Prelang Builder <builder@prelang.com>
2016-04-15 13:13:30 +0000

    Installed relation 'has_many' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
   has_many :activity_logs
+  has_many :project_files
 end
