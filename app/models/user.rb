56cc745d60d1b8f6c3cc69f35912ae83bfc6c68e
Prelang Builder <builder@prelang.com>
2016-04-15 13:13:30 +0000

    Defined validation for attribute 'fax_number' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
   has_many :projects
   belongs_to :bid
   validates_formatting_of :phone_number, using: :us_phone
+  validates_formatting_of :fax_number, using: :us_phone
 end
